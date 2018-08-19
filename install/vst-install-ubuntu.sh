3c49e0b25cdae4ea45caf36e96113d4d36859cf8
Gowthamraj Vungarala <info@gowthamraj.in>
2018-08-20 03:16:02 +0530

    Update vst-install-ubuntu.sh

--- install/vst-install-ubuntu.sh
+++ install/vst-install-ubuntu.sh
@@ -1298,7 +1298,7 @@ echo -e "#----------------------------------------------------------#"
 apt install -y software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:ondrej/php
 apt update
-apt-get install php7.2-apcu php7.2-mbstring php7.2-bcmath php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-mysql php7.2-soap php7.2-xml php7.2-zip php7.2-memcache php7.2-memcached php7.2-zip
+apt-get install -y php7.2-apcu php7.2-mbstring php7.2-bcmath php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-mysql php7.2-soap php7.2-xml php7.2-zip php7.2-memcache php7.2-memcached php7.2-zip
 update-rc.d php7.2-fpm defaults
 a2enconf php7.2-fpm
 service apache2 restart
